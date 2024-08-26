4cdbb6863a857708f27b6c91465c1c7ab4a04d72
Tomas Roun <tomas.roun8@gmail.com>
2024-08-26 23:26:54 +0200

    Rename codec

--- pyjsx/codecs.py
+++ pyjsx/codecs.py
@@ -4,18 +4,18 @@ import encodings
 from pyjsx.transpiler import transpile
 
 
-def pyjsx_decode(input: bytes, errors: str = "strict") -> tuple[str, int]:  # noqa: A002, ARG001
-    return transpile(input.decode("utf-8")), len(input)
+def pyjsx_decode(input: memoryview, errors: str = "strict") -> tuple[str, int]:  # noqa: A002, ARG001
+    return transpile(bytes(input).decode("utf-8")), len(input)
 
 
 def pyjsx_search_function(encoding: str) -> codecs.CodecInfo | None:
-    if encoding != "pyjsx":
+    if encoding != "jsx":
         return None
 
     utf8 = encodings.search_function("utf8")
     assert utf8 is not None
     return codecs.CodecInfo(
-        name="pyjsx",
+        name="jsx",
         encode=utf8.encode,
         decode=pyjsx_decode,
         incrementalencoder=utf8.incrementalencoder,
