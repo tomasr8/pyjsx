363a9c2b113a05d891672406c1483f10a09c3b6d
Tomas Roun <tomas.roun8@gmail.com>
2025-01-26 11:52:57 +0100

    Add tests

---
+++ tests/test_import_hook.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import pytest
+
+from pyjsx.import_hook import PyJSXFinder, register_import_hook, unregister_import_hook
+
+
+@pytest.fixture
+def import_hook():
+    register_import_hook()
+    yield
+    unregister_import_hook()
+
+
+def test_finder():
+    finder = PyJSXFinder()
+    path = str(Path(__file__).parent / "test_module")
+    spec = finder.find_spec("main", [path])
+    assert spec is not None
+    assert spec.name == "main"
+
+
+@pytest.mark.usefixtures("import_hook")
+def test_import():
+    from .test_module import main
+
+    assert str(main.hello()) == """\
+<h1>
+    Hello, World!
+</h1>"""
